ef614373c5d25c8cb75cc550ad2115d7495fca7b
Vojta Biberle <vojtech.biberle@gmail.com>
2015-06-18 07:58:08 +0200

    Securely add API-KEYS

---
+++ test/test_simple.php
@@ -0,0 +1,23 @@
+<?php
+
+include('../vendor/autoload.php');
+include('api_keys.php');
+
+$client = new Mailchimp\Client(APIKEY);
+
+$accountDetails = $client->getAccountDetails();
+
+$subscriberListCollection = $client->getLists();
+
+$subscriberList = $client->getList(LIST_ID);
+
+$jocho = $client->getMember(LIST_ID, md%(TEST_EMAIL));
+
+$members = $subscriberList->getMembers();
+
+$member = $members->findMembers(['email_address' => TEST_EMAIL]);
+
+$member[0]->setEmailAddress('email');
+$member[0]->save();
+
+//$subscriberList->addMember(new \Mailchimp\Message\Members());
